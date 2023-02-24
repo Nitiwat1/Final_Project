21833afa9885559c19b083f3f4d91f902fa6226e
Nitiwat1 <nitiwat9989@gmail.com>
2023-02-24 13:39:06 +0700

    Finalv1

--- routes/shop.js
+++ routes/shop.js
@@ -2,6 +2,8 @@ var express = require('express');
 var router = express.Router();
 const shopController = require('../controllers/shopController')
 const { body } = require('express-validator');
+const passportJWT = require("../middleware/passportJWT").isLogin;
+const checkAdmin = require("../middleware/checkAdmin").isAdmin;
 
 /* GET home page. */
 router.get('/', shopController.shop);
@@ -10,6 +12,6 @@ router.get('/:id', shopController.show);
 router.post('/', [
     body("name").not().isEmpty().withMessage("กรุณาใส่ชื่อร้านด้วย"),
 ], shopController.insert);
-router.delete('/:id',shopController.delete)
+router.delete('/:id',[passportJWT, checkAdmin],shopController.delete)
 
 module.exports = router;
